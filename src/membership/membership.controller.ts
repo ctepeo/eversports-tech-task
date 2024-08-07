e3fe642987fc8b86e227aab2a64018e2873f68ff
Egor Sazanovich <egor@sazanovi.ch>
2024-08-07 02:14:39 +0200

    missing types added

--- src/membership/membership.controller.ts
+++ src/membership/membership.controller.ts
@@ -3,17 +3,22 @@ import { MembershipService } from './membership.service';
 
 import { Membership, MembershipPeriod } from './membership.interface';
 
+interface MembershipWithPeriods {
+  membership: Membership;
+  periods: MembershipPeriod[] | [];
+}
+
 @Controller()
 export class MembershipController {
   @Get() // GET /membership
-  public async list(): Promise<object[]> {
-    const memberships = MembershipService.getMemberships();
-    const membershipPeriods = MembershipService.getMembershipPeriods();
+  public async list(): Promise<MembershipWithPeriods[] | []> {
+    const memberships: Membership[] | [] = MembershipService.getMemberships();
+    const membershipPeriods: MembershipPeriod[] | [] = MembershipService.getMembershipPeriods();
 
-    return memberships.map((membership: Membership) => {
+    return memberships.map((membership: Membership): MembershipWithPeriods => {
       return {
         membership,
-        periods: membershipPeriods.filter((period: MembershipPeriod) => period.membershipId === membership.id),
+        periods: membershipPeriods.filter((period: MembershipPeriod): boolean => period.membershipId === membership.id),
       };
     });
   }
--- src/membership/membership.service.ts
+++ src/membership/membership.service.ts
@@ -4,15 +4,11 @@ import membershipPeriods from '../../test/data/membership-periods.json';
 import { Membership, MembershipPeriod } from './membership.interface';
 
 export class MembershipService {
-  public static getMemberships(): Membership[] {
-    return membership;
+  public static getMemberships(): Membership[] | [] {
+    return membership || [];
   }
 
-  public static getMembershipPeriods(): MembershipPeriod[] {
-    return membershipPeriods;
+  public static getMembershipPeriods(): MembershipPeriod[] | [] {
+    return membershipPeriods || [];
   }
-
-  // public static getMembershipPeriodsByMembershipId(membershipId: number): object {
-  //   return membership.find((m: any) => m.id === id);
-  // }
 }
